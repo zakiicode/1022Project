2169c2d32edca7614dd4f12acf55958d6ecf897d
zakiicode <92188960+zakiicode@users.noreply.github.com>
2022-03-29 22:25:04 -0400

    Update MainActivity.java

--- zakibeyblade/MainActivity.java
+++ zakibeyblade/MainActivity.java
@@ -2,17 +2,19 @@ package com.example.beybladespin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.util.Log;
+import android.os.Handler;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
-import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         timer(true);
+
     }
     //private double rand;
     private int round = 1;
@@ -28,15 +31,36 @@ public class MainActivity extends AppCompatActivity {
     private boolean newRound = false;
     CountDownTimer timer;
     private int score =0;
+    private int barSize =0;
+    DisplayMetrics displayMetrics = new DisplayMetrics();
+
 
     public void timer(boolean onoroff){
         timer = new CountDownTimer(20000, 1000){
             TextView timerView = (TextView) findViewById(R.id.timer);
+            Button barView=(Button) findViewById(R.id.timerbar);
+            ViewGroup.LayoutParams layoutParams = barView.getLayoutParams();
+
 
             public void onTick(long millisUntilFinished) {
                 NumberFormat f = new DecimalFormat("00");
                 long sec = (millisUntilFinished / 1000) % 60;
                 timerView.setText(f.format(sec));
+                getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+                int width = displayMetrics.widthPixels;
+                barSize+=(width/20);
+                layoutParams.width=barSize;
+                if(barSize <= width/3){
+                    barView.setBackgroundColor(Color.GREEN);
+                }
+                else if(barSize >width/3 && barSize<= 2*(width/3)){
+                    barView.setBackgroundColor(Color.YELLOW);
+                }
+                else{
+                    barView.setBackgroundColor(Color.RED);
+
+                }
+                barView.setLayoutParams(layoutParams);
             }
 
             public void onFinish() {
@@ -73,52 +97,91 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void letitrip(View v){
-
-        TextView rorwView= (TextView) findViewById(R.id.rorw);
-        rorwView.setVisibility(View.INVISIBLE);
-        Button contView=(Button) findViewById(R.id.cont);
-        contView.setVisibility(View.INVISIBLE);
-        TextView messageView= (TextView) findViewById(R.id.message);
-        messageView.setVisibility(View.INVISIBLE);
         EditText guessView=(EditText) findViewById(R.id.guess);
-        guessView.setVisibility(View.INVISIBLE);
+        String gV = guessView.getText().toString();
+        Button lirView = (Button) findViewById(R.id.lir);
 
-        Button lirView=(Button) findViewById(R.id.lir);
-        lirView.setVisibility(View.INVISIBLE);
-        TextView sureView= (TextView) findViewById(R.id.sure);
-        sureView.setVisibility(View.VISIBLE);
-        Button yesView=(Button) findViewById(R.id.yes);
-        yesView.setVisibility(View.VISIBLE);
-        Button noView=(Button) findViewById(R.id.no);
-        noView.setVisibility(View.VISIBLE);
+        if (!(gV.equals(""))) {
+            TextView rorwView = (TextView) findViewById(R.id.rorw);
+            rorwView.setVisibility(View.INVISIBLE);
+            Button contView = (Button) findViewById(R.id.cont);
+            contView.setVisibility(View.INVISIBLE);
+            TextView messageView = (TextView) findViewById(R.id.message);
+            messageView.setVisibility(View.INVISIBLE);
+            TextView betView = (TextView) findViewById(R.id.between);
+            betView.setVisibility(View.INVISIBLE);
+            guessView.setVisibility(View.INVISIBLE);
 
+            lirView.setVisibility(View.INVISIBLE);
+            TextView sureView = (TextView) findViewById(R.id.sure);
+            sureView.setVisibility(View.VISIBLE);
+            Button yesView = (Button) findViewById(R.id.yes);
+            yesView.setVisibility(View.VISIBLE);
+            Button noView = (Button) findViewById(R.id.no);
+            noView.setVisibility(View.VISIBLE);
+        }
+        else {
+            lirView.setError("guess cannot be null or a number that's 0-10");
+
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+
+                    lirView.setError(null);
+                }
+            }, 3000);   //5 seconds
+
+        }
     }
     public void no(View v){
-        if(newRound){
-            timer.start();
-            newRound=false;
+        if(round<=9) {
+            if (newRound) {
+                timer.start();
+                round++;
+                barSize = 0;
+                newRound = false;
+            }
+            TextView rorwView = (TextView) findViewById(R.id.rorw);
+            rorwView.setVisibility(View.INVISIBLE);
+            Button contView = (Button) findViewById(R.id.cont);
+            contView.setVisibility(View.INVISIBLE);
+            TextView messageView = (TextView) findViewById(R.id.message);
+            messageView.setVisibility(View.VISIBLE);
+            TextView betView = (TextView) findViewById(R.id.between);
+            betView.setVisibility(View.VISIBLE);
+            EditText guessView = (EditText) findViewById(R.id.guess);
+            guessView.setVisibility(View.VISIBLE);
+
+            Button lirView = (Button) findViewById(R.id.lir);
+            lirView.setVisibility(View.VISIBLE);
+            TextView sureView = (TextView) findViewById(R.id.sure);
+            sureView.setVisibility(View.INVISIBLE);
+            Button yesView = (Button) findViewById(R.id.yes);
+            yesView.setVisibility(View.INVISIBLE);
+            Button noView = (Button) findViewById(R.id.no);
+            noView.setVisibility(View.INVISIBLE);
         }
-        TextView rorwView= (TextView) findViewById(R.id.rorw);
-        rorwView.setVisibility(View.INVISIBLE);
-        Button contView=(Button) findViewById(R.id.cont);
-        contView.setVisibility(View.INVISIBLE);
-        TextView messageView= (TextView) findViewById(R.id.message);
-        messageView.setVisibility(View.VISIBLE);
-        EditText guessView=(EditText) findViewById(R.id.guess);
-        guessView.setVisibility(View.VISIBLE);
+        else{
+            TextView rorwView= (TextView) findViewById(R.id.rorw);
+            rorwView.setVisibility(View.INVISIBLE);
+            Button contView=(Button) findViewById(R.id.cont);
+            contView.setVisibility(View.VISIBLE);
+            TextView sureView= (TextView) findViewById(R.id.sure);
+            sureView.setVisibility(View.INVISIBLE);
+            Button yesView=(Button) findViewById(R.id.yes);
+            yesView.setVisibility(View.INVISIBLE);
+            Button noView=(Button) findViewById(R.id.no);
+            noView.setVisibility(View.INVISIBLE);
 
-        Button lirView=(Button) findViewById(R.id.lir);
-        lirView.setVisibility(View.VISIBLE);
-        TextView sureView= (TextView) findViewById(R.id.sure);
-        sureView.setVisibility(View.INVISIBLE);
-        Button yesView=(Button) findViewById(R.id.yes);
-        yesView.setVisibility(View.INVISIBLE);
-        Button noView=(Button) findViewById(R.id.no);
-        noView.setVisibility(View.INVISIBLE);
-
+        }
 
     }
     public void yes(View v){
+        Button barView=(Button) findViewById(R.id.timerbar);
+        ViewGroup.LayoutParams layoutParams = barView.getLayoutParams();
+        int speed = layoutParams.width;
+        layoutParams.width=0;
+        barView.setLayoutParams(layoutParams);
         timer.cancel();
         TextView timerView = (TextView) findViewById(R.id.timer);
 
@@ -138,12 +201,28 @@ public class MainActivity extends AppCompatActivity {
         EditText guessView=(EditText) findViewById(R.id.guess);
         double gV = Double.parseDouble(guessView.getText().toString());
         TextView scoreView= (TextView) findViewById(R.id.score);
-
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int width = displayMetrics.widthPixels;
         if (gV == 10){
             rorwView.setText("YESSIR");
             right++;
             newRound=true;
             score = score + 100;
+            if(speed<=width/5){
+                score+=50;
+            }
+            else if(speed>width/5 && speed<=2*(width/5)){
+                score+=30;
+            }
+            else if(speed>2*(width/5) && speed<=3*(width/5)){
+                score+=15;
+            }
+            else if(speed>3*(width/5) && speed<=4*(width/5)){
+                score+=7;
+            }
+            else{
+                score+=2;
+            }
             scoreView.setText("SCORE: "+score);
         }
         else{
